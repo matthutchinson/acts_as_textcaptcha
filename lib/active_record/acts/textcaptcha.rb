073ddf54ce69e6c874766a78023a4bc372ecc7fa
Matthew Hutchinson <matt@hiddenloop.com>
2010-04-16 12:14:40 +0100

    adding bycrypt answer encryption options

--- lib/active_record/acts/textcaptcha.rb
+++ lib/active_record/acts/textcaptcha.rb
@@ -13,14 +13,10 @@ module ActiveRecord
     module Textcaptcha #:nodoc:
 
       def acts_as_textcaptcha(options = nil)
-        cattr_accessor :config, :textcaptcha_salt
+        cattr_accessor :config
         attr_accessor  :spam_answer, :spam_question, :possible_answers
                                 
-        self.textcaptcha_salt = BCrypt::Engine.generate_salt
-        
-        if options.is_a?(String)
-          self.config = {'api_key' => options}
-        elsif options.is_a?(Hash) && options['api_key']
+        if options.is_a?(Hash) && options['api_key'] && options['salt']
           self.config = options
         else
           begin
@@ -40,7 +36,7 @@ module ActiveRecord
 
         def allowed; true end
 
-        def validate
+        def validate     
           if new_record?
             if allowed
               errors.add(:spam_answer, 'is incorrect, try another question instead') unless validate_spam_answer
@@ -50,25 +46,35 @@ module ActiveRecord
           end
         end     
         
-        def validate_spam_answer
+        def validate_spam_answer            
           return true  if skip_spam_check
-          return false if !spam_answer || !possible_answers
+          return false if !spam_answer || !possible_answers    
+          
+          possible_answers.each do |poss|
+            puts "poss => #{poss}"
+          end
+          
           possible_answers.include?(encrypt_answer(Digest::MD5.hexdigest(spam_answer.strip.downcase.to_s)))
         end
-        
-        def encrypt_answer(answer)
-          BCrypt::Engine.hash_secret(answer, textcaptcha_salt)
+         
+        def encrypt_answers(answers)   
+          answers.map {|answer| encrypt_answer(answer) }
         end
-
+           
+        def encrypt_answer(answer)          
+          return answer unless(config['bcrypt_salt'])
+          BCrypt::Engine.hash_secret(answer, config['bcrypt_salt'], (config['bcrypt_cost'] || 10))
+        end            
+        
         def generate_spam_question(use_textcaptcha = true)
           if use_textcaptcha && config && config['api_key']
             begin
               resp = Net::HTTP.get(URI.parse('http://textcaptcha.com/api/'+config['api_key']))
               if !resp.empty? && xml = XML::Parser.string(resp).parse
                 self.spam_question    = xml.find('/captcha/question')[0].inner_xml
-                self.possible_answers = xml.find('/captcha/answer').map(&:inner_xml)
+                self.possible_answers = encrypt_answers(xml.find('/captcha/answer').map(&:inner_xml))
               end
-              return possible_answers.map {|ans| encrypt_answer(ans) } if spam_question && !possible_answers.empty?
+              return possible_answers if spam_question && !possible_answers.empty?
             rescue SocketError, Timeout::Error, Errno::EINVAL, Errno::ECONNRESET, EOFError, Errno::ECONNREFUSED,
                    Net::HTTPBadResponse, Net::HTTPHeaderSyntaxError, Net::ProtocolError, URI::InvalidURIError => e
               log_textcaptcha("failed to load or parse textcaptcha with key '#{config['api_key']}'; #{e}")
@@ -80,9 +86,9 @@ module ActiveRecord
             log_textcaptcha('falling back to random logic question from config')
             random_question       = config['questions'][rand(config['questions'].size)]
             self.spam_question    = random_question['question']
-            self.possible_answers = random_question['answers'].split(',').map!{|ans| Digest::MD5.hexdigest(ans)}
-          end
-          possible_answers.map {|ans| encrypt_answer(ans) }
+            self.possible_answers = encrypt_answers(random_question['answers'].split(',').map!{|ans| Digest::MD5.hexdigest(ans)})
+          end     
+          possible_answers
         end
 
         private
