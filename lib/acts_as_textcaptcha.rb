8bdf6c4ed93b131d0966e83be836529d9b470a38
Matthew Hutchinson <matt@hiddenloop.com>
2014-01-02 21:36:34 +0000

    including version.rb

--- lib/acts_as_textcaptcha.rb
+++ lib/acts_as_textcaptcha.rb
@@ -1,3 +1,4 @@
+require 'acts_as_textcaptcha/version'
 require 'acts_as_textcaptcha/textcaptcha'
 require 'acts_as_textcaptcha/textcaptcha_helper'
 require 'acts_as_textcaptcha/framework/rails' if defined?(Rails)
